5d6372d7f2e59b8b3b24602ec90693426f6c9dc6
Ashvattha Gautam Gole <ashvatthagole@gmail.com>
2026-03-05 04:19:02 +0530

    Complete S-type instruction encoding implementation

--- S_type.py
+++ S_type.py
@@ -38,6 +38,13 @@ def s_type(instruction):
 
     #parsing 
     part1=instruction.split()
+    instruction_name = part1[0]
+
+    # instruction validation
+    if instruction_name not in s_type_info:
+        print("invalid instruction")
+        return
+    
     part2=part1[1].split(",")
     rs2=part2[0]
     base_offset=part2[1].strip(")").split("(")
@@ -61,7 +68,26 @@ def s_type(instruction):
         print("the immediate can't be represented using a 12 bit 2's complement representation")
         return
     
+    #converting the immediate to 12 bit 2's complement form 
+    if int_imm>=0:
+        imm_binary=(bin(int_imm))[2:]
+        immediate="0"*(12-len(imm_binary))+imm_binary
+    else:
+        int_imm = 2**12 + int_imm
+        imm_binary=(bin(int_imm))[2:]
+        immediate="0"*(12-len(imm_binary))+imm_binary
+
+    #splitting the intermediate 
+    imm_11_5=immediate[:7]
+    imm_4_0=immediate[7:]
     
+    #final binary instruction
+    # S-type format: imm[11:5] | rs2 | rs1 | funct3 | imm[4:0] | opcode
+    binary_instruction = imm_11_5 + REGISTER_MAPPING[rs2] + REGISTER_MAPPING[rs1] + s_type_info[instruction_name]["funct3"] + imm_4_0 + s_type_info[instruction_name]["opcode"]
+    return binary_instruction
+
+
+
 
 
 
